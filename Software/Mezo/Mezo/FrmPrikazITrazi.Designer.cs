1b3171456c2804bfa27162b9e25264a5b85bb2a8
Danijela Stjepanović <dstjepano22@foi.hr>
2024-05-30 17:18:00 +0200

    FrmPrikaziITrazi promijenila sam izgled i nazive forme

---
+++ Software/Mezo/Mezo/FrmPrikazITrazi.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Mezo
+{
+    partial class FrmPrikazITrazi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTrazi = new System.Windows.Forms.Label();
+            this.txtTrazi = new System.Windows.Forms.TextBox();
+            this.dgvPregledRecenzija = new System.Windows.Forms.DataGridView();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnAzuriraj = new System.Windows.Forms.Button();
+            this.btnBrisi = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPregledRecenzija)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTrazi
+            // 
+            this.lblTrazi.AutoSize = true;
+            this.lblTrazi.Location = new System.Drawing.Point(22, 104);
+            this.lblTrazi.Name = "lblTrazi";
+            this.lblTrazi.Size = new System.Drawing.Size(78, 13);
+            this.lblTrazi.TabIndex = 0;
+            this.lblTrazi.Text = "Traži recenziju:";
+            // 
+            // txtTrazi
+            // 
+            this.txtTrazi.Location = new System.Drawing.Point(106, 101);
+            this.txtTrazi.Name = "txtTrazi";
+            this.txtTrazi.Size = new System.Drawing.Size(152, 20);
+            this.txtTrazi.TabIndex = 1;
+            // 
+            // dgvPregledRecenzija
+            // 
+            this.dgvPregledRecenzija.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPregledRecenzija.Location = new System.Drawing.Point(296, 24);
+            this.dgvPregledRecenzija.Name = "dgvPregledRecenzija";
+            this.dgvPregledRecenzija.Size = new System.Drawing.Size(476, 375);
+            this.dgvPregledRecenzija.TabIndex = 2;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Location = new System.Drawing.Point(38, 167);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(99, 42);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            // 
+            // btnAzuriraj
+            // 
+            this.btnAzuriraj.Location = new System.Drawing.Point(38, 238);
+            this.btnAzuriraj.Name = "btnAzuriraj";
+            this.btnAzuriraj.Size = new System.Drawing.Size(99, 41);
+            this.btnAzuriraj.TabIndex = 4;
+            this.btnAzuriraj.Text = "Ažuriraj";
+            this.btnAzuriraj.UseVisualStyleBackColor = true;
+            // 
+            // btnBrisi
+            // 
+            this.btnBrisi.Location = new System.Drawing.Point(38, 306);
+            this.btnBrisi.Name = "btnBrisi";
+            this.btnBrisi.Size = new System.Drawing.Size(99, 40);
+            this.btnBrisi.TabIndex = 5;
+            this.btnBrisi.Text = "Briši";
+            this.btnBrisi.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrikazITrazi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBrisi);
+            this.Controls.Add(this.btnAzuriraj);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.dgvPregledRecenzija);
+            this.Controls.Add(this.txtTrazi);
+            this.Controls.Add(this.lblTrazi);
+            this.Name = "FrmPrikazITrazi";
+            this.Text = "FrmPrikazITrazi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPregledRecenzija)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTrazi;
+        private System.Windows.Forms.TextBox txtTrazi;
+        private System.Windows.Forms.DataGridView dgvPregledRecenzija;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnAzuriraj;
+        private System.Windows.Forms.Button btnBrisi;
+    }
+}
--- Software/Mezo/Mezo/FrmPrikazITrazi.cs
+++ Software/Mezo/Mezo/FrmPrikazITrazi.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace Mezo
 {
-    public partial class Form1 : Form
+    public partial class FrmPrikazITrazi : Form
     {
-        public Form1()
+        public FrmPrikazITrazi()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
