14817d9b45b241e09c757c20d5a38b43979644f6
Danijela Stjepanović <dstjepano22@foi.hr>
2024-05-30 17:57:04 +0200

    klase izrađene klase KonzumiranaJela i Recenzija

---
+++ Software/Mezo/Mezo/Models/KonzumiranaJela.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mezo.Models
+{
+    public class KonzumiranaJela
+    {
+        public string Id_Jelo { get; set; }
+        public string GlavnoJelo { get; set;}
+        public string VrijemeNarudzbe {  get; set; }
+
+        public string VrijemeUrucivanja { get; set; }
+
+    }
+}
---
+++ Software/Mezo/Mezo/Models/Recenzija.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mezo.Models
+{
+    public class Recenzija
+    {
+        public string Id_Recenzija { get; set; }
+        public string DatumRecenzije { get; set; }
+        public string OcjenaOkusa { get; set; }
+        public string OcjenaKolicina { get; set; }
+        public string Komentar { get; set; }
+
+        public string Id_Jelo { get; set; }
+    }
+}
